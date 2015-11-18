1cf56239afaa6106b69d649f8fb0f90358a7f6d3
kgshukla <kgshukla@yahoo.com>
2015-11-18 23:11:24 +0800

    test

--- poc-app/test/summer-test.js
+++ poc-app/test/summer-test.js
@@ -3,6 +3,6 @@ var summer = require('../')
 
 test('summer', function (t) {
       var winter = summer(-40)
-      t.equal(winter(40), 0, 'should be equal')
+      t.equal(winter(40), 1, 'should be equal')
       t.end()
 })
